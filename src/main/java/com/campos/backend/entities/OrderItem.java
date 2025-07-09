c189bfcfc7806851547458fac3d74f1aafa68ac1
mateusribeirocampos <mateusrcampos@gmail.com>
2025-07-09 17:46:20 -0300

    feat: Product-OrderItem one-to-many association

--- src/main/java/com/campos/backend/entities/OrderItem.java
+++ src/main/java/com/campos/backend/entities/OrderItem.java
@@ -15,15 +15,15 @@ import jakarta.persistence.Table;
 public class OrderItem implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@EmbeddedId
 	private OrderItemPK id = new OrderItemPK();
-	
+
 	private Integer quantity;
 	private Double price;
-	
+
 	public OrderItem() {
-		
+
 	}
 
 	public OrderItem(Order order, Product product, Integer quantity, Double price) {
@@ -33,20 +33,21 @@ public class OrderItem implements Serializable {
 		this.quantity = quantity;
 		this.price = price;
 	}
-	
+
 	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
-	
+
 	public void setOrder(Order order) {
 		id.setOrder(order);
 	}
-	
+
+
 	public Product getProduct() {
 		return id.getProduct();
 	}
-	
+
 	public void setProduct(Product product) {
 		id.setProduct(product);
 	}
@@ -83,7 +84,5 @@ public class OrderItem implements Serializable {
 		OrderItem other = (OrderItem) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
 
 }
