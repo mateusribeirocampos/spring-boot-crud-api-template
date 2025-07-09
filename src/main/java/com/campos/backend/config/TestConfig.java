c1a53c6b9763c5a42e9400d21cd61f63ee181dff
mateusribeirocampos <mateusrcampos@gmail.com>
2025-07-09 18:12:47 -0300

    feat: Payment, one-to-one association

--- src/main/java/com/campos/backend/config/TestConfig.java
+++ src/main/java/com/campos/backend/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.campos.backend.entities.Category;
 import com.campos.backend.entities.Order;
 import com.campos.backend.entities.OrderItem;
+import com.campos.backend.entities.Payment;
 import com.campos.backend.entities.Product;
 import com.campos.backend.entities.User;
 import com.campos.backend.entities.enums.OrderStatus;
@@ -82,6 +83,10 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
