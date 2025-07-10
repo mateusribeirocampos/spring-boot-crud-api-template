a90e9c73da1cdd6461af7821a77d29caf3bb0972
mateusribeirocampos <mateusrcampos@gmail.com>
2025-07-10 15:32:58 -0300

    feat: Subtotal & total methods

--- src/main/java/com/campos/backend/entities/OrderItem.java
+++ src/main/java/com/campos/backend/entities/OrderItem.java
@@ -67,12 +67,16 @@ public class OrderItem implements Serializable {
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+	
+	public Double getSubTotal() {
+		return price * quantity;
+	}
 
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
