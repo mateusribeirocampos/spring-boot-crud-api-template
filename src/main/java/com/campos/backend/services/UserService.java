72db75dd57e5439c486719ae5c6b666c98e559f2
mateusribeirocampos <mateusrcampos@gmail.com>
2025-07-11 17:47:57 -0300

    feat: Exception handling - delete

--- src/main/java/com/campos/backend/services/UserService.java
+++ src/main/java/com/campos/backend/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.campos.backend.entities.User;
 import com.campos.backend.repositories.UserRepository;
+import com.campos.backend.services.exceptions.DatabaseException;
 import com.campos.backend.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,9 +33,16 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+
 	}
-	
+
 	public User update(Long id, User obj) {
 		User entity = repository.getReferenceById(id); // Prepara o objeto monitorado
 		updateData(entity, obj);
