17a2e90926f42d19e0feab9b9c37534e822de262
mateusribeirocampos <mateusrcampos@gmail.com>
2025-07-13 19:50:07 -0300

    chore: dotenv in the bakendApplication and application properties ready to prod

--- src/main/java/com/campos/backend/BackendApplication.java
+++ src/main/java/com/campos/backend/BackendApplication.java
@@ -3,10 +3,17 @@ package com.campos.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		// Carregar variáveis do .env
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> 
+			System.setProperty(entry.getKey(), entry.getValue()));
+		
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
